eba5faf4c57455fa3512e6402aa6408299b94ee0
mayconjmorais <mayconjmorais@gmail.com>
2022-08-23 21:00:47 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-module "network" {
+resource "virtual_network" "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
